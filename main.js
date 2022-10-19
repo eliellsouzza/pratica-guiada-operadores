e3016208efd788ebd9837b6fbd8e7468973d9a98
Eliel Souza <eliellsouzza@gmail.com>
2022-10-19 19:29:28 -0300

    exercicios- operadores-eliel

---
+++ main.js
@@ -0,0 +1,17 @@
+let saldo = 15 + 22.40
+console.log("saldo", saldo)
+
+saldo = saldo - 12.34
+console.log("Saldo atualizado", saldo)
+
+saldo = saldo / 3
+console.log ("valor por irmão", saldo)
+
+saldo = saldo * 3.42
+console.log ("Rendimento", saldo)
+
+
+saldo = saldo %  3
+console.log("chicletinho",saldo)
+
+
