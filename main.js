5bf6abcc00b2cd4f9efcbc52ca4174caa759be1b
Eliel Souza <eliellsouzza@gmail.com>
2022-10-19 19:55:34 -0300

    atividade sendo realizada

--- main.js
+++ main.js
@@ -12,6 +12,34 @@ console.log ("Rendimento", saldo)
 
 
 saldo = saldo %  3
-console.log("chicletinho",saldo)
+console.log("chicletinho saldo irmãos",saldo)
+
+
+const multiplicaESoma = 2*2+2
+console.log("MS1", multiplicaESoma)
+
+const multiplicaESoma2 = 2*(2+2)
+console.log("MS2", multiplicaESoma2)
+
+
+// = atribuição
+// == valor
+// === valor e tipo
+// !== valor e tipo diferente
+
+const comparacaoIgual = 5 === 5 
+const comparacaoDiferente = 5 !== 5
+console.log("5 !== 5", comparacaoDiferente)
+
+
+const comparacaotipo1 = 5 !== "5"
+console.log ("5 !== '5'", comparacaotipo1)
+
+const comparacaoigual1 = 5===5
+console.log ("5===5", comparacaoigual1)
+
+
+const comparacaotipo2 = "5" === "cinco"
+console.log("5" === "cinco", comparacaotipo2)
 
 
